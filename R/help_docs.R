14135430f8f36a5a1924b515d19b841cb74a76d5
Ben Tupper <btupper@bigelow.org>
2025-01-04 20:47:23 -0500

    autocommit 2025-01-04T20:47:23

--- R/help_docs.R
+++ R/help_docs.R
@@ -5,3 +5,15 @@
 update_help_docs = function(path = "~/Library/CloudStorage/Dropbox/code/projects/ColbyForecastingDocs"){
   source(file.path(path, "inst/update_script.R"))
 }
+
+
+install_docs = function(repos = "BigelowLab/ColbyForecastingDocs", ...){
+  #' Install the Colby Forecasting Documentation package
+  #' 
+  #' Please see \href{https://github.com/BigelowLab/ColbyForecastingDocs}{ColbyForecastingDocs}
+  #' @param repos chr, the namespace and name of the package on github
+  #' @param ... other arguments for \code{\link[remotes]{install_github}}
+  #' @return see \code{\link[remotes]{install_github}}
+  
+  remotes::install_github(repos, ...)
+}
